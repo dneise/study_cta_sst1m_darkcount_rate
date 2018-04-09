e0e06e302d65e7ab286009ca2c1c3adecc4531d2
Dominik Neise <neised@phys.ethz.ch>
2018-04-09 21:45:23 +0200

    initial commit

---
+++ spe_analysis.sh
@@ -0,0 +1,9 @@
+rm -rf SST1M01_20171124.00?.h5
+digicam-spe --pulse_finder_threshold=3.0 -c SST1M01_20171124.000.h5 SST1M01_20171124.000.fits.fz &
+digicam-spe --pulse_finder_threshold=3.0 -c SST1M01_20171124.001.h5 SST1M01_20171124.001.fits.fz &
+digicam-spe --pulse_finder_threshold=3.0 -c SST1M01_20171124.002.h5 SST1M01_20171124.002.fits.fz &
+digicam-spe --pulse_finder_threshold=3.0 -c SST1M01_20171124.003.h5 SST1M01_20171124.003.fits.fz &
+digicam-spe --pulse_finder_threshold=3.0 -c SST1M01_20171124.004.h5 SST1M01_20171124.004.fits.fz &
+digicam-spe --pulse_finder_threshold=3.0 -c SST1M01_20171124.005.h5 SST1M01_20171124.005.fits.fz &
+digicam-spe --pulse_finder_threshold=3.0 -c SST1M01_20171124.006.h5 SST1M01_20171124.006.fits.fz &
+digicam-spe --pulse_finder_threshold=3.0 -c SST1M01_20171124.007.h5 SST1M01_20171124.007.fits.fz &
